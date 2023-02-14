a63ae3db568830f265acd508719d1e158f78662a
BB497 <45710478+BB497@users.noreply.github.com>
2023-02-14 20:32:49 +0900

    123

---
+++ Study/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;               // namespace System을 사용한다.
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Study             // namespace 이름 선언.
+{                           // namespace 의 블럭
+	class Program           // class는 객체, class (이름)으로 선언
+	{
+		// Main 함수 시작
+		static void Main(string[] args)		 // 메소드(Method): 한정자(static), 반환타입(void), method의 이름(Main)
+		{
+			// Write, WriteLine: 전달받은 인자를 출력. WriteLine은 줄바꿈도 해줌
+			Console.Write("Hello World!\n");
+			Console.WriteLine("Hello World!\n");
+			//Console.ReadKey();              // ReadKey: 키 입력을 기다리는 메소드
+
+			// 문제
+			// "여러분, ", "안녕하세요?", "반갑습니다!"
+
+			// 콘솔창에 출력 결과
+			// 여러분, 안녕하세요?
+			// 반갑습니다!
+			Console.Write("여러분, ");
+			Console.WriteLine("안녕하세요?");
+			Console.Write("반갑습니다!\n");
+			Console.ReadKey(); 
+		}
+	}
+}
