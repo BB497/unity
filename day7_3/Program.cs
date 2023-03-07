b1c39a7d9efa1b27ec6cbde493eff3eca70c39c9
BB497 <45710478+BB497@users.noreply.github.com>
2023-03-07 21:20:25 +0900

    day7_4

---
+++ day7_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 다차원 배열
+			// 3차원 배열 선언 및 초기화.
+			int[,,] arr = new int[4, 3, 2]
+			{//					  z, y, x
+				{ // z: 0
+				 // x 0 1
+					{ 1,2},		// y: 0
+					{ 3,4},		// y: 1
+					{ 5,6}		// y: 2
+				},
+				{  // z: 1
+					{ 7,8},
+					{ 9,10},
+					{ 11,12}
+				},
+				{  // z: 2
+					{ 13,14},
+					{ 15,16} ,
+					{ 17,18} 
+				},
+				{   // z: 3
+					{ 19,20},
+					{ 21,22},
+					{ 23,24} 
+				}
+			};
+
+			for(int z = 0; z < arr.GetLength(0); z++)
+			{
+				Console.Write("{ \n");
+				for(int y = 0; y < arr.GetLength(1); y++)
+				{
+					Console.Write("   { ");
+					for(int x = 0; x < arr.GetLength(2); x++)
+					{
+						if (x == 0)
+							Console.Write("{0}, ", arr[z, y, x]);
+						else
+							Console.Write("{0}, ", arr[z, y, x]);
+					}
+					Console.WriteLine("}");
+				}
+				Console.WriteLine("}");
+			}
+		}
+	}
+}
---
+++ day7_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 가변 배열
+			//		들쭉날쭉한 배열이라는 의미
+			//		크기가 정해져 있지 않다.
+
+			// 선언
+			int[][] jArr = new int[2][];
+
+			jArr[0] = new int[10];      // y=0인 곳에 길이가 10인 배열
+			jArr[1] = new int[5];       // y=1인 곳에 길이가 5인 배열.
+
+			// 가변 배열의 사용 예제(선언 및 초기화, 사용)
+			int[][] jArr2 = new int[3][];
+
+			jArr2[0] = new int[5] { 1, 2, 3, 4, 5 };        // 배열 크기 지정
+			jArr2[1] = new int[] { 10, 20, 30 };            // 배열 크기 자동
+			jArr2[2] = new int[] { 100, 200 };
+
+			for (int i = 0; i < jArr2.Length; i++)		// jArr2의 배열 길이
+			{
+				for (int j = 0; j < jArr2[i].Length; j++)
+					Console.Write("{0} ", jArr2[i][j]);
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			foreach (int[] array in jArr2)
+			{
+				foreach (int n in array)
+					Console.Write("{0} ", n);
+				Console.WriteLine();
+			}
+		}
+	}
+}
