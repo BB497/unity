3ab293a2dd9f03da75bbb0afc19f28c877ef7118
BB497 <45710478+BB497@users.noreply.github.com>
2023-03-07 20:26:59 +0900

    day7

---
+++ Day7/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//	- 인스턴스
+			//		배열_변수.Length: 배열의 길이 반환
+			//		배열_변수.GetLength(치수); : 배열의 길이 반환 (다차원 배열)
+			//		배열_변수.Rank;			: 배열의 차원 반환 (몇차원인지)
+
+			int[] scores = { 40, 30, 60, 73, 80, 50 };      // 크기 6의 int형 배열
+
+			Console.WriteLine("====배열의 모든 요소 출력====");
+			foreach (int score in scores)
+				Console.Write("{0} ", score);
+			Console.WriteLine();
+
+			Console.WriteLine("====오름차순 정렬 후 출력====");
+			Array.Sort(scores);		// 오름차순으로 정렬.
+			Array.ForEach(scores, PrintNum);
+			Console.WriteLine();
+
+			Console.WriteLine("{0}차원 배열입니다.", scores.Rank);
+			Console.WriteLine();
+
+			int idx = Array.IndexOf(scores, 80);
+			Console.WriteLine("80점인 요소의 인덱스: {0}", idx);
+
+			Console.WriteLine("모든 인원이 통과했는지 여부: {0}", Array.TrueForAll(scores, CheckPassed));
+
+			// 60점보다 낮은 요소 모두 81점으로 올리기.
+			for(int i = 0; i < scores.Length; i++)
+			{
+				if (!CheckPassed(scores[i]))
+					scores[i] = 81;
+			}
+			Console.WriteLine("모든 인원이 통과했는지 여부: {0}", Array.TrueForAll(scores, CheckPassed));
+
+			int oldLength = scores.Length;
+			Array.Resize(ref scores, 10);			// 배열의 크기를 10으로 변경.
+			Console.WriteLine("배열의 길이가 {0}에서 {1}로 변경됨.", oldLength, scores.Length);
+
+			Console.WriteLine("====배열의 모든 요소 출력====");
+			foreach (int score in scores)
+				Console.Write("{0} ", score);
+			Console.WriteLine();
+
+			Console.WriteLine("====2번부터 5개의 인덱스를 클리어 후 출력====");
+			Array.Clear(scores, 2, 5);
+			foreach(int score in scores)
+			{
+				Console.Write("{0} ", score);
+			}
+			Console.WriteLine();
+		}
+
+		static void PrintNum(int num)
+		{
+			Console.Write("{0} ", num);
+		}
+
+		static bool CheckPassed(int score)
+		{
+			return score >= 60;				// score가 60이상이면 true, 아니면 false반환
+		}
+	}
+}
---
+++ Day7_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// - 인스턴스
+			//		배열_변수.GetLength(치수);	: 배열의 길이 반환(다차원 배열)
+
+
+			// 2차원 배열
+			int[,] arr = new int[3, 4];
+			arr[0, 0] = 1;
+			arr[0, 1] = 2;
+			arr[0, 2] = 3;
+			arr[0, 3] = 4;
+			arr[1, 0] = 5;
+			arr[1, 1] = 6;
+			arr[1, 2] = 7;
+			arr[1, 3] = 8;
+			arr[2, 0] = 9;
+			arr[2, 1] = 10;
+			arr[2, 2] = 11;
+			arr[2, 3] = 12;
+
+
+			// 크기가 4인 1차원 배열 요소를 3개 가지는 2차원 배열.
+			int[,] arr1 = new int[3, 4] { { 1, 2, 3, 4 }, 
+										  { 5, 6, 7, 8 },
+										  { 9, 10, 11, 12 } };
+			
+			foreach (int n in arr1)				// 2차원 배열의 요소 순차적으로 변수 n에 저장.
+				Console.Write("{0} ", n);
+			Console.WriteLine();
+
+			for(int y = 0; y<arr1.GetLength(0); y++)	// 가장 앞의 차원 수를 반환
+			{
+				for(int x = 0; x < arr1.GetLength(1); x++)		// 두번째 차원 수를 반환.
+				{
+					Console.Write("[y: {0}, x:{1}]: {2}\t", y, x, arr1[y, x]);
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("Length: {0}", arr1.Length);
+			Console.WriteLine("Rank: {0}", arr1.Rank);
+			Console.WriteLine("GetLength(0): {0}", arr1.GetLength(0));		// 3
+			Console.WriteLine("GetLength(1): {0}", arr1.GetLength(1));		// 4
+		}
+	}
+}
